5669792059b13fb6c40cf761c4f50cbc5c225ece
dexeonify <bloonyapprentice@gmail.com>
2021-10-21 15:18:13 +0800

    'Compile av1an.sh' now automatically git pull
    Managed to make 'MSYS2 MinGW' work in Windows Terminal. :D
    In Windows Terminal, go to Settings > Add a new profile and copy the
    command line into the appropriate text box. Adjust to your needs.
    
    If you have installed git on Windows, you can add -use-full-path to the
    command line so that MinGW can also access your Windows PATH variable.
    
    Have a look at https://www.msys2.org/docs/terminals/.

--- Compile av1an.sh
+++ Compile av1an.sh
@@ -1,13 +1,10 @@
 # A script to automatically compile av1an because I'm lazy :P
 # Drag and drop this script to 'MSYS2 MinGW' to run.
-#
-# Run this beforehand to update repo
-# cd D:\Av1an\
-# git pull
+
+cd /d/Av1an
+git pull
 
 pacman -Syu
-
-cd /d/Av1an/
 cargo build --release --no-default-features
 strip target/release/av1an.exe
 
