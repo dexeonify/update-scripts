7822721ddcb097b1fbf196cf5c7509e3de62022a
dexeonify <bloonyapprentice@gmail.com>
2022-01-02 23:50:16 +0800

    Only sort binaries that gets updated
    - Remove `cjpeg.exe`
      mozjpeg and mozjpeg-tran hasn't been updated in a while
      (https://github.com/mozilla/mozjpeg), so they are already staying in
      `Image/` most of the time.
    
    - Remove `curl.exe`, "geckodriver.exe", "wget.exe"
      These are updated separately when needed or if I feel like it. Since I
      download them manually, I will naturally put them in the correct folder.

--- Sort Binaries.ps1
+++ Sort Binaries.ps1
@@ -11,8 +11,8 @@ $folders = @{
 $categories = @{
     "video" = "aomenc.exe", "av1an.exe", "mediainfo.exe", "rav1e.exe", "SvtAv1EncApp.exe", "vpxenc.exe"
     "audio" = "opusenc.exe", "fdkaac.exe"
-    "image" = "avifenc.exe", "avifdec.exe", "cavif.exe", "cjxl.exe", "djxl.exe", "cwebp.exe", "cjpeg.exe", "gifski.exe"
-    "others" = "aria2c.exe", "croc.exe", "curl.exe", "geckodriver.exe", "wget.exe"
+    "image" = "avifenc.exe", "avifdec.exe", "cavif.exe", "cjxl.exe", "djxl.exe", "cwebp.exe", "gifski.exe"
+    "others" = "aria2c.exe", "croc.exe"
     "ffmpeg" = "ffmpeg.exe", "ffplay.exe", "ffprobe.exe", "avcodec-59.dll", "avdevice-59.dll", "avfilter-8.dll",
                "avformat-59.dll", "avutil-57.dll", "postproc-56.dll", "swresample-4.dll", "swscale-6.dll"
 }
