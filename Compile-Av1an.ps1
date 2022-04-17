61170f2c46a14a57f526474565a411e1876fa544
dexeonify <bloonyapprentice@gmail.com>
2022-04-17 21:48:35 +0800

    Update av1an compile script
    The previous MSYS-based compile_av1an.sh script is now replaced with a
    new Visual Studio-based Compile-Av1an.ps1 script, to reflect the
    official compilation guide added in master-of-zen/Av1an@3675f05.
    
    The biggest benefit to using the Visual Studio method is that I can now
    compile av1an with ffmpeg 5.0. That's not possible with MSYS due to
    msys2/MINGW-packages#10725.
    
    I don't plan to remove Compile_av1an.md because it is still a viable
    alternative. Both options are just as involved so pick your poison :)

---
+++ Compile-Av1an.ps1
@@ -0,0 +1,18 @@
+﻿<#
+    .SYNOPSIS
+    A script to automatically compile av1an because I'm lazy :P
+    Now using Visual Studio.
+
+    .LINK
+    All prerequisites and installation guide here:
+    https://github.com/master-of-zen/Av1an#compilation-on-windows
+#>
+Push-Location "D:\Av1an"
+
+git pull
+cargo rustc --release -- -C target-cpu=skylake
+Copy-Item ".\target\release\av1an.exe" "D:\Programs\Video"
+cargo clean
+
+Pop-Location
+Read-Host -Prompt "Finished compiling Av1an"
